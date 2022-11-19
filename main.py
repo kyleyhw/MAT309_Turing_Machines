2a9aca2410799e698e0787f7f847c31beed4598f
Kyle <kyleyhw@gmail.com>
2022-11-19 15:44:16 -0500

    updated interpretation to include more than single digit states added fill out tape with '0's to set length moved print_output into Machine class updated printing to align with multiple digit states added invalid instruction error message added crash error message added filename variable added Bilianuk example 10.5 added ps2 q3a, ps2 q3b, ps2 q3c first drafts

--- main.py
+++ main.py
@@ -12,45 +12,55 @@ class Machine():
             instruction = row[2]
         else:
             print('ERROR')
-            return
+            return None
 
-        if len(instruction) != 3:
-            position = -1
+        if instruction == 'BLANK':
+            position = -2
             print('HALTED')
             return (state, tape, position)
 
-        (new_value, move, new_state) = tuple(instruction)
+        (new_value, move, new_state) = (instruction[0], instruction[1], instruction[2:])
         (new_value, move, new_state) = (int(new_value), move, int(new_state))
 
         tape[position] = new_value
 
         if move == 'R':
             position +=1
-        if move == 'L':
+        elif move == 'L':
             position -=1
+        else:
+            print('INVALID INSTRUCTION')
+            return None
 
         return (int(new_state), tape, position)
+
+    def print_output(self, state, tape, position):
+        state = str(state).rjust(max([len(x[0]) for x in self.table]), ' ')
+        tape = merge_list_to_string(tape)
+        print(state + ':' + tape[:position] + '\033[4m' + tape[position] + '\033[0m' + tape[position + 1:])
 
 
 def merge_list_to_string(list):
     list = ''.join(str(x) for x in list)
     return list
 
-def print_output(state, tape, position):
-    state = str(state)
-    tape = merge_list_to_string(tape)
-    print(state + ':' + tape[:position] + '\033[4m' + tape[position] + '\033[0m' + tape[position + 1:])
 
 
-table = np.loadtxt('quiz_7.txt', dtype=str, delimiter=',')
+
+filename = 'ps2_q3b.txt'
+table = np.loadtxt(filename, dtype=str, delimiter=',')
 machine = Machine(table)
 
-tape = '011110'
+tape = '011011'
+tape = tape.ljust(8,'0')
 tape = np.array([int(x) for x in list(tape)])
 
 (state, tape, position) = (1, tape, 0)
 
+print(filename.rstrip('.txt'))
 print('input: ' + merge_list_to_string(tape))
 while position > -1:
-    print_output(state, tape, position)
+    machine.print_output(state, tape, position)
     (state, tape, position) = machine.interpret_instruction(state=state, tape=tape, position=position)
+if position == -1:
+    print('CRASHED')

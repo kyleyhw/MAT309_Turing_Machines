b289187f71d7bd9e60d0deda311a72c8fe633763
Kyle <kyleyhw@gmail.com>
2022-11-19 13:34:26 -0500

    final push for quiz 7, before ps2

--- main.py
+++ main.py
@@ -14,12 +14,10 @@ class Machine():
             print('ERROR')
             return
 
-
         if len(instruction) != 3:
             position = -1
             print('HALTED')
             return (state, tape, position)
-
 
         (new_value, move, new_state) = tuple(instruction)
         (new_value, move, new_state) = (int(new_value), move, int(new_state))
@@ -33,6 +31,7 @@ class Machine():
 
         return (int(new_state), tape, position)
 
+
 def merge_list_to_string(list):
     list = ''.join(str(x) for x in list)
     return list
@@ -43,13 +42,11 @@ def print_output(state, tape, position):
     print(state + ':' + tape[:position] + '\033[4m' + tape[position] + '\033[0m' + tape[position + 1:])
 
 
-
-table = np.loadtxt('table.txt', dtype=str, delimiter=',')
+table = np.loadtxt('quiz_7.txt', dtype=str, delimiter=',')
 machine = Machine(table)
 
 tape = '011110'
 tape = np.array([int(x) for x in list(tape)])
-
 
 (state, tape, position) = (1, tape, 0)
 

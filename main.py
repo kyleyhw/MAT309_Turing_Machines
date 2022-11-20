41c85822fafaae965c741f302475468ec66fdb45
Kyle <kyleyhw@gmail.com>
2022-11-19 19:02:05 -0500

    added show_triple option to Machine.print_output finished ps2_q3d

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from machine import Machine
 from misc_functions import merge_list_to_string
 
 
-filename = 'ps2_q3b.txt'
+filename = 'ps2_q3d.txt'
 table = np.loadtxt('machines/' + filename, dtype=str, delimiter=',')
 machine = Machine(table)
 
-tape = '011011'
-tape = tape.ljust(8,'0')
+tape = '011111'
+tape = tape.ljust(len(tape) + 2,'0')
 tape = np.array([int(x) for x in list(tape)])
 
 (state, tape, position) = (1, tape, 0)
@@ -16,7 +16,7 @@ tape = np.array([int(x) for x in list(tape)])
 print(filename.rstrip('.txt'))
 print('input: ' + merge_list_to_string(tape))
 while position > -1:
-    machine.print_output(state, tape, position)
+    machine.print_output(state, tape, position, show_triple=False)
     (state, tape, position) = machine.interpret_instruction(state=state, tape=tape, position=position)
 if position == -1:
     print('CRASHED')
